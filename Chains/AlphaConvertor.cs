bfba5a25e2f3e95f18054abd3ed913f77781688a
PYTNAG <sgds.2020.nag@yandex.ru>
2024-04-23 00:03:31 +0500

    Remove unnecessary class AlphaConvertor

--- Chains/AlphaConvertor.cs
+++
@@ -1,9 +0,0 @@
-namespace Chains;
-
-internal static class AlphaConvertor
-{
-    public static Chain Convert(Alpha matrixAlpha)
-    {
-        return BetaProducer.Beta(matrixAlpha.RandomGene());
-    }
-}
