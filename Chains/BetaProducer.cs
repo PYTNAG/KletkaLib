3976cd8be7682ad8ffd235aeee0336f0b249b735
PYTNAG <sgds.2020.nag@yandex.ru>
2024-04-22 23:24:13 +0500

    Add optional parameter for custom transcript function in beta producer class

--- Chains/BetaProducer.cs
+++ Chains/BetaProducer.cs
@@ -2,12 +2,22 @@ namespace Chains;
 
 public static class BetaProducer
 {
-    public static Chain GetBeta(Alpha matrix)
+    /// <summary>
+    /// Construct chain based on given alpha matrix
+    /// </summary>
+    /// <param name="matrix">Alpha chain, genotype</param>
+    /// <param name="transcription">Transcripts alpha's body to beta's body. If null then using default transcription which just inverse bytes</param>
+    /// <returns>New transcripted chain</returns>
+    public static Chain GetBeta(Alpha matrix, Func<byte[], byte[]>? transcription = null)
     {
-        byte[] beta = matrix.RandomGene()
-            .Select(b => (byte)~b)
-            .ToArray();
-
+        byte[] beta = transcription is null ? DefaultTranscription(matrix.RandomGene()) : transcription(matrix.RandomGene());
         return new(Alpha.GeneInitialSequence, beta);
+    }
+
+    private static byte[] DefaultTranscription(byte[] gene)
+    { 
+        return gene
+                .Select(b => (byte)~b)
+                .ToArray();
     }
 }
