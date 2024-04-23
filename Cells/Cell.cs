6370e4ddf1e10ec99295848b0cc5cc81657b2189
PYTNAG <sgds.2020.nag@yandex.ru>
2024-04-23 19:36:06 +0500

    Add cell class

---
+++ Cells/Cell.cs
@@ -0,0 +1,30 @@
+using Chains;
+
+namespace Cells;
+
+public abstract class Cell
+{
+    public event Action<IEnumerable<Chain>>? ChainsReleased;
+    public event Action<Cell>? CloneSplitted;
+    public event Action<Cell>? Died;
+
+    protected readonly Alpha _genotype;
+    public byte[] Genotype => _genotype.Body;
+
+    protected readonly HashSet<Chain> _freeChains; // includes betas and gammas
+
+    public Cell(Alpha? genotype = null)
+    {
+        _genotype = genotype ?? new();
+        _freeChains = new();
+    }
+
+    public abstract void Tick(HashSet<Chain> neighbourhoodChains, Energy energy);
+
+    protected void ReleaseChains(IEnumerable<Chain> chains) => ChainsReleased?.Invoke(chains);
+    
+    protected abstract Cell GetClone();
+    protected void SplitClone() => CloneSplitted?.Invoke(GetClone());
+
+    protected void Die() => Died?.Invoke(this);
+}
