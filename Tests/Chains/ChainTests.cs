7087e678aa794b91cc4bb6f075b7baf17ce69d54
PYTNAG <sgds.2020.nag@yandex.ru>
2024-05-04 08:22:57 +0500

    Add test for random constructor

--- Tests/Chains/ChainTests.cs
+++ Tests/Chains/ChainTests.cs
@@ -5,6 +5,19 @@ namespace Tests.Chains;
 public sealed class ChainTests
 {
     [Fact]
+    public void Constructor_RandomLength()
+    {
+        int
+            headLength = Random.Shared.Next(),
+            bodyLength = Random.Shared.Next();
+
+        Chain chain = new(headLength, bodyLength);
+
+        Assert.Equal(chain.Head.Length, headLength);
+        Assert.Equal(chain.Body.Length, bodyLength);
+    }
+
+    [Fact]
     public void TryAccept_AcceptableChain_ReturnsTrue()
     {
         Chain chain = new(1, 1);
